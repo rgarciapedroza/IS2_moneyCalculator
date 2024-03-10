b99f58deff9566f8ea62c7b2aad9a6bb6b80e2f6
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-03-10 19:27:59 +0000

    Fixed buttons on sidebar completely, JS for clicking works now

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+/*document.addEventListener('DOMContentLoaded', function(){
     const buttonHome = document.querySelector(".homeTab");
     const buttonProfile = document.querySelector(".profileTab");
     
@@ -15,4 +15,33 @@ document.addEventListener('DOMContentLoaded', function(){
         buttonProfile.classList.add('clicked');
         buttonHome.classList.add('not-clicked');
     }
-});
+});*/
+
+//document.addEventListener('onload', configTabs);
+
+configTabs();
+
+function configTabs() {
+    const buttons = document.querySelectorAll(".sidebar-tab");
+    
+    buttons.forEach(button => {
+
+        button.addEventListener("click", function() {
+            console.log("AAAAAAAAAAA");
+            // Remove 'clicked' class from all buttons
+            buttons.forEach(btn => {
+                btn.classList.remove('clicked');
+            });
+            
+            // Add 'clicked' class to the clicked button
+            this.classList.add('clicked');
+            
+            // Remove 'not-clicked' class from all buttons except the clicked one
+            buttons.forEach(btn => {
+                if (btn !== this) {
+                    btn.classList.remove('not-clicked');
+                }
+            });
+        });
+    });
+}
