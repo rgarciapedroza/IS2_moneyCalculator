71b3f30e5c410ad89e5ec39f86026ff9ec4e92e3
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-03-27 17:48:14 +0000

    Added code to script.js to dynamically change all classes in DOM tree when certain conditions meet, supports any number of different screen names and conditions to apply them, but pnly supports one for now

--- js/script.js
+++ js/script.js
@@ -10,5 +10,44 @@ window.onload = function () {
     login.addEventListener('click', () => {
         formContainer.classList.remove('active');
     })
+
+    let currentDevice;
+    var deviceTypes = ['desktop', 'mobile'];
+    function determineClass() {
+        var screenWidth = window.innerWidth;
+        if (screenWidth > 390) {
+            return 'desktop';
+        } else {
+            return 'mobile';
+        }
+    }
+
+    function applyResponsiveClasses() {
+        let buffer = determineClass();
+        if (buffer !== currentDevice) {
+            currentDevice = buffer.slice();
+            let others = deviceTypes.slice();
+            others.splice(deviceTypes.indexOf(currentDevice), 1);
+    
+            // Iterate through all elements in the DOM
+            var allElements = document.getElementsByTagName('*');
+            for (var i = 0; i < allElements.length; i++) {
+                var element = allElements[i];
+                element.classList.add(currentDevice);
+                for (var j in others) {
+                    element.classList.remove(others[j]);
+                }
+            }
+        }
+    }
+    
+    // Call applyResponsiveClasses function on page load
+    applyResponsiveClasses();
+    
+    // Add event listener for window resize event
+    window.addEventListener('resize', function() {
+        // Call applyResponsiveClasses function whenever window is resized
+        applyResponsiveClasses();
+    });
 }
 
