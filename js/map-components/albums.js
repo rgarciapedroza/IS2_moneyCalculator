da46a64912e6654e21394d4a269331f1f229f809
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-03-30 21:24:48 +0000

    Implemented album and image JSON handling functions, there's some client-side code that I think is not precisely great to have there

---
+++ js/map-components/albums.js
@@ -0,0 +1,99 @@
+import {v4 as uuidv4} from 'uuid';
+
+export function imageBufferToBlob(buffer) {
+    const blob = new Blob([buffer]);
+    const blobUrl = URL.createObjectURL(blob);
+    return blobUrl;
+}
+
+export async function imageBlobToBuffer(blob) {
+    const response = await fetch(blobUrl);
+    if (!response.ok) {
+        throw new Error('Error with blob image link');
+    }
+    return await response.arrayBuffer();
+}
+
+export function extractImageDate(file) {
+    return new Date(file.lastModified);
+}
+
+export function imageFileToBufferArray(image) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+        const buffer = event.target.result;
+        resolve(buffer);
+        };
+        reader.onerror = function(error) {
+        reject(error);
+        };
+        reader.readAsArrayBuffer(image);
+    });
+}
+
+export function generateImageJSON(ID="No image ID", filename="No filename", date='No date', description='No description', image=new ArrayBuffer()) {
+    let returner = {
+        id: ID,
+        filename: filename,
+        date: date,
+        description: description,
+        image: image,
+    };
+    return returner;
+}
+
+export function generateAlbumJSON(name='No user name', userID='No user ID', albumID='No album ID', datestart='No start date', dateend='No end date', cityname='No city', coordinates=[0.0,0.0], description='No description', images={}) {
+    let buffer = [];
+    let returner = {
+        name: name,
+        userID: userID,
+        albumID: albumID,
+        datestart: datestart,
+        dateend: dateend,
+        cityname: cityname,
+        coordinates: coordinates,
+        description: description,
+        images: images,
+    };
+
+    return returner;
+}
+
+//I get the feeling that defining UUIDs client-side is the mother of all bad ideas
+export function addImage(album, imageJSON, imageID=album["albumID"] + '-' + uuidv4()) {
+    let newImage = imageJSON;
+    newImage["ID"] = imageID;
+    album["images"][imageID] = newImage;
+}
+
+export function removeImage(album, ID) {
+    if (album["images"] && album["images"].hasOwnProperty(ID)) {
+        delete album["images"][ID];
+        return true;
+    }
+    return false;
+}
+
+export function getImage(album, ID) {
+    return album["images"][ID];
+}
+
+export function replaceImage(album, replacedID, iNewImage) {
+    let newImage = iNewImage;
+    newImage["ID"] = replacedID;
+    album["images"][replacedID] = newImage;
+}
+
+export function dictToJSON(dict) {
+    return JSON.stringify(dict);
+}
+
+export function JSONToDict(json) {
+    try {
+        return JSON.parse(json);
+    } catch (ex) {
+        console.error(ex);
+        return null;
+    }
+}
